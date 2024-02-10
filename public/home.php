f568c7512e44fc902a384831707e00f649cda5ba
arfat132 <arfatbegum90@gmail.com>
2024-02-10 11:29:31 +0300

    [Done] header design

---
+++ public/home.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en" data-bs-theme="auto">
+
+<head>
+    <script src="../assets/js/color-modes.js"></script>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <title>Home · Bootstrap v5.3</title>
+
+    <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/headers.css" rel="stylesheet">
+</head>
+
+<body>
+    <main>
+        <!-- header -->
+        <header class="p-3 mb-3 border-bottom bg-dark">
+            <div class="container">
+                <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                    <a href="/" class="h5 text-white fw-bold pt-1 text-decoration-none">
+                   The Story Sage
+                    </a>
+
+                    <ul class="nav ms-4 col-12 col-lg-auto me-lg-auto mb-2 justify-content-center text-white mb-md-0">
+                        <li><a href="#" class="nav-link px-2 link-body-emphasis text-white">Post</a></li>
+                        <li><a href="#" class="nav-link px-2 link-body-emphasis text-white">Customers</a></li>
+                        <li><a href="#" class="nav-link px-2 link-body-emphasis text-white">Contact</a></li>
+                    </ul>
+
+                    <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+                        <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
+                    </form>
+
+                    <div class="dropdown text-end">
+                        <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle text-white" data-bs-toggle="dropdown" aria-expanded="false">
+                            <img src="https://static.vecteezy.com/system/resources/thumbnails/024/095/208/small/happy-young-man-smiling-free-png.png" alt="mdo" width="32" height="32" class="rounded-circle">
+                        </a>
+                        <ul class="dropdown-menu text-small">
+                            <li><a class="dropdown-item" href="#">New project...</a></li>
+                            <li><a class="dropdown-item" href="#">Settings</a></li>
+                            <li><a class="dropdown-item" href="#">Profile</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Log out</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </header>
+    </main>
+    <script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
