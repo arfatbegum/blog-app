4d9af3d8083af64a8fae6a455514b9b47d019b38
arfat132 <arfatbegum90@gmail.com>
2024-02-10 12:31:33 +0300

    [DONE] carousel

--- public/home.php
+++ public/home.php
@@ -10,8 +10,7 @@
     <title>Home · Bootstrap v5.3</title>
 
     <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 
     <!-- Custom styles for this template -->
     <link href="assets/css/headers.css" rel="stylesheet">
@@ -20,7 +19,7 @@
 <body>
     <main>
         <!-- header -->
-        <header class="p-3 mb-3 border-bottom bg-dark">
+        <header class="p-3 border-bottom bg-dark">
             <div class="container">
                 <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                     <a href="/" class="h5 text-white fw-bold pt-1 text-decoration-none">
@@ -54,6 +53,44 @@
                 </div>
             </div>
         </header>
+
+        <!-- carousel -->
+        <div id="carouselExampleDark" class="carousel carousel-dark slide">
+            <div class="carousel-inner">
+                <div class="carousel-item active" data-bs-interval="10000">
+                    <img src="assets/images/carousel-image-3.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block bg-dark text-white p-4">
+                        <i class="bi bi-quote h1"></i>
+                        <h5 class="mt-2 fw-bold">Discover the World Through Words: Your Ultimate Blogging Destination</h5>
+                        <p>Immerse yourself in a world of captivating narratives, insightful perspectives, and engaging discussions. Explore diverse topics, share your stories, and connect with a global community of passionate bloggers on our vibrant platform.</p>
+                    </div>
+                </div>
+                <div class="carousel-item" data-bs-interval="2000">
+                    <img src="assets/images/carousel-image-2.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block bg-dark text-white p-4">
+                        <i class="bi bi-quote h1"></i>
+                        <h5 class="mt-2 fw-bold">Unleash Your Creativity: Where Ideas Meet Expression</h5>
+                        <p>Ignite your creativity and unleash your imagination on our dynamic blogging platform. From thought-provoking articles to creative endeavors, our space is your canvas to express, inspire, and innovate. Join a supportive community and let your voice be heard.</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="assets/images/carousel-image-1.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block bg-dark text-white p-4">
+                        <i class="bi bi-quote h1"></i>
+                        <h5 class="mt-2 fw-bold">Empower Your Voice: Amplify Your Impact with Our Blogging Community</h5>
+                        <p>Empower yourself and others through the power of words. Join a community of like-minded individuals dedicated to making a difference. Whether you're sharing personal experiences, advocating for change, or exploring new horizons, our platform is your launchpad to amplify your voice and effect positive change.</p>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
 
         <!-- footer -->
         <div class="container">
