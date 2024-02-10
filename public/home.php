48d8a017b2dcf9cf0cd2ab57da0bce1f2c6cc198
arfat132 <arfatbegum90@gmail.com>
2024-02-10 11:57:32 +0300

    [DONE] footer design

--- public/home.php
+++ public/home.php
@@ -24,7 +24,7 @@
             <div class="container">
                 <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                     <a href="/" class="h5 text-white fw-bold pt-1 text-decoration-none">
-                   The Story Sage
+                        The Story Sage
                     </a>
 
                     <ul class="nav ms-4 col-12 col-lg-auto me-lg-auto mb-2 justify-content-center text-white mb-md-0">
@@ -54,6 +54,53 @@
                 </div>
             </div>
         </header>
+
+        <!-- footer -->
+        <div class="container">
+            <footer class="py-5">
+                <div class="row">
+                    <div class="col-6 col-md-2 mb-3">
+                        <h5>Section</h5>
+                        <ul class="nav flex-column">
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-6 col-md-2 mb-3">
+                        <h5>Section</h5>
+                        <ul class="nav flex-column">
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-6 col-md-2 mb-3">
+                        <h5>Section</h5>
+                        <ul class="nav flex-column">
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
+                            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-md-5 offset-md-1 mb-3">
+                        <form>
+                            <h5>Subscribe to our newsletter</h5>
+                            <p>Monthly digest of what's new and exciting from us.</p>
+                            <div class="d-flex flex-column flex-sm-row w-100 gap-2">
+                                <label for="newsletter1" class="visually-hidden">Email address</label>
+                                <input id="newsletter1" type="text" class="form-control" placeholder="Email address">
+                                <button class="btn btn-dark" type="button">Subscribe</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <p class="text-center border-top pt-4">&copy; 2023 Company, Inc. All rights reserved.</p>
+            </footer>
+        </div>
     </main>
     <script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
 
